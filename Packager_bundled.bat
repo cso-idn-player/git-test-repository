b5a0b07a8016cc9f31201cf700739b3d8c83cacf
Sak karepku yho! <61906629+cso-idn-player@users.noreply.github.com>
2021-04-17 04:42:14 +0700

    Add files via upload

---
+++ Packager_bundled.bat
@@ -0,0 +1,40 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWH3eyH0FOx5GXzisPXizIboO7eb8086ItUMRW+M7a7Pe1LecOfAH70Gqd58jtg==
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJhSA==
+::ZR4luwNxJguZRRmM4FYgO0EbAwGMcnyzDbwM7ezy7qSIsEpSQ+s0ccDj07qEKeEd7Xrte4IoxHRXncUYMxhZcwK4Dg==
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJGax50ciOyd6TReLG2SoA7sb1Mz0+OSLrUwZR94PeYzQ27OLMt8R/0vgeZMpmH9Cnas=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+ECHO Decrypting...
+COPY encrypted.bin decrypted.mp4
+ECHO Extracting...
+start "" "decrypted.mp4"
+ECHO Done!
